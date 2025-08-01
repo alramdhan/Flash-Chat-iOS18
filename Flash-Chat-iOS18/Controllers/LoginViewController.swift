f6cf49750b8289c432acb8466674493b51a3921c
Dika <al.ramdhan312@gmail.com>
2025-08-01 17:00:34 +0700

    Section 15: 186 Done

--- Flash-Chat-iOS18/Controllers/LoginViewController.swift
+++ Flash-Chat-iOS18/Controllers/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
@@ -13,5 +14,14 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func loginPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print(e.localizedDescription)
+                } else {
+                    self.performSegue(withIdentifier: "LoginToChat", sender: self)
+                }
+            }
+        }
     }
 }
